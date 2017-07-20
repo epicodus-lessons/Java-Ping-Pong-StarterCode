28bd20a2d5877168073433c72da5b4ab06727a98
Perry Eising <perry.r.eising@gmail.com>
2017-07-20 09:24:11 -0700

    completed Red, Green, Refactor with JUnit

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 public class PingPong {
 
     public ArrayList<Object> runPingPong(int countUpTo){
-        return null;
+        ArrayList<Object> result = new ArrayList<Object>();
+        for (int i = 1; i <= countUpTo; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                result.add("pingpong");
+            } else if (i % 3 == 0){
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
     }
-
-
 }
--- src/test/java/models/PingPongTest.java
+++ src/test/java/models/PingPongTest.java
@@ -14,4 +14,58 @@ public class PingPongTest {
         expectedOutput.add(1);
         assertEquals(expectedOutput, testPingPong.runPingPong(1)); //i may be red
     }
+
+    @Test
+    public void runPingPong_countUpToGivenNumber_ArrayList() throws Exception {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        assertEquals(expectedOutput, testPingPong.runPingPong(2));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf3_ArrayList() throws Exception {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        assertEquals(expectedOutput, testPingPong.runPingPong(3));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf5_ArrayList() throws Exception {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(5));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOfBoth3And5_ArrayList() throws Exception {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        expectedOutput.add("ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("ping");
+        expectedOutput.add("pong");
+        expectedOutput.add(11);
+        expectedOutput.add("ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("pingpong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(15));
+    }
+
 }
