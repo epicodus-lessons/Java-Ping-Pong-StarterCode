436f805d7ff94f2d48cfe124172f1dce053e2d44
Perry Eising <perry.r.eising@gmail.com>
2017-07-20 14:33:01 -0700

    completed Frontend for PingPong

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,24 @@
-public class App {
+import models.PingPong;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("I'm a ping-pong application!");
+        System.out.println("Enter a number:");
+        try {
+            String stringUserNumber = bufferedReader.readLine();
+            int intUserNumber = Integer.parseInt(stringUserNumber);
+            PingPong pingPong = new PingPong();
+            ArrayList<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
+            System.out.println(pingPongResult);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
